3d2a3d402e56d8b7575ccbbef501341c5e5e2512
aaron <ajwills@umich.edu>
2026-04-15 15:32:33 -0400

    loose leaf tear

--- src/components/SeeMoreCard.tsx
+++ src/components/SeeMoreCard.tsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
+import TornEdge, { useTornClip } from "./TornEdge";
 
 const cardClassName =
-  "flex items-center justify-center w-[320px] min-w-[320px] snap-start pl-11 pr-5 pt-5 pb-5 notebook-card shadow-[2px_3px_8px_rgba(0,0,0,0.06)] transition-all duration-200 hover:shadow-[3px_5px_12px_rgba(0,0,0,0.1)] hover:-translate-y-0.5";
+  "flex items-center justify-center w-[320px] min-w-[320px] snap-start pl-11 pr-5 pt-5 pb-5 notebook-card transition-all duration-200 hover:-translate-y-0.5";
 
 export default function SeeMoreCard() {
+  const clipStyle = useTornClip("see-more");
   return (
     <Link
       to="/experience#projects"
       className={cardClassName}
+      style={clipStyle}
     >
+      <TornEdge seed="see-more" />
       <span className="text-lg">see more &rarr;</span>
     </Link>
   );
