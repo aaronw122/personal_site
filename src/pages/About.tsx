8a1a9d80170ecda0d7545e6aed5733db5cbfc1db
aaron <ajwills@umich.edu>
2026-05-01 19:32:56 -0400

    new about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import usePageTitle from "../hooks/usePageTitle";
 
 export default function About() {
@@ -6,21 +5,29 @@ export default function About() {
   return (
     <div>
       <h2>about</h2>
-
       <div className="prose about-prose">
         <p>hey! i'm aaron.</p>
         <p>
-          aspiring renaissance man. engineer currently building at fractal nyc.
+          i grew up in chicago in a family of 5, and spent a ton of time with my nose in a book.
+          so much so that i got a detention in the second grade for reading too much.
         </p>
         <p>
-          spent two years as a PM at expedia building products that drove $7M
-          in annual profit. switched to engineering because i wanted to build
-          things instead. check out my projects{" "}
-          <Link to="/experience#projects" className="link-underline">here.</Link>
+          at age 14, i started caddying at evanston golf club. caddying had a big impact on me -
+          it exposed me to people from all different backgrounds, and taught me that charisma beats smarts.
+          i also got a full ride to the university of michigan as an evans scholar.
         </p>
         <p>
-          i live for learning new things, the outdoors, meeting new people, and
-          live music.
+          in college, i worked on a tarpit idea to build an app to bridge the political divide.
+          we pivoted to a newsletter, and failed miserably, but i learned a lot. mainly how to lead teams,
+          and that i was interested in tech.
+        </p>
+        <p>
+          i spent 2 years at expedia as a pm building products that drove $7m in annual profit.
+          started teaching myself to program in the spring of 2025 because i wanted to build things!
+        </p>
+        <p>
+          currently, i spend my time at fractal nyc working on a project that helps people refine their web design.
+          i also spend my time meditating, meeting new people, and catching live music when i can.
         </p>
         <p>
           i'd describe most of my friends as misfits. i'm drawn to people who
@@ -31,7 +38,6 @@ export default function About() {
           </a>
         </p>
       </div>
-
       <p className="about-quote">
         as a child i wanted to believe the magic in the books i read was real.
         its not, but turns out tech is a close second.
